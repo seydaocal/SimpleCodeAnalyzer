a81cbcafdf0414060f2c4c8f735241a2b38509f8
seydaocal <seyda.ocal77@gmail.com>
2026-02-21 21:27:54 +0300

    Initial commit - Code Analyzer project

---
+++ Analyzer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+class Analyzer
+{
+    public void Analyze(string[] files)
+    {
+        int totalLines = 0;
+        int commentLines = 0;
+
+        Dictionary<string, int> keywords = new Dictionary<string, int>();
+        keywords["if"] = 0;
+        keywords["for"] = 0;
+        keywords["while"] = 0;
+
+        foreach (string file in files)
+        {
+            Console.WriteLine("\nBeing analyzing: " + file);
+            string[] lines = File.ReadAllLines(file); // read the all line in the file.
+            foreach (string line in lines)
+            {
+                string trimmed = line.Trim();
+                if (trimmed != "") //skip the empty lines.
+                {
+                    totalLines++;
+
+                    if (trimmed.StartsWith("//")) //count the number of comment lines.
+                    {
+                        commentLines++;
+                    }
+                    foreach (var key in keywords.Keys)//count the each keyword in the code.
+                    {
+                        if (line.Contains(key))
+                        {
+                            keywords[key]++;
+                        }
+                    }
+                }
+            }
+        }
+        Console.WriteLine("\nTotal lines of code: " + totalLines);
+        Console.WriteLine("\nTotal comment lines of code: " + commentLines);
+        foreach (var pair in keywords)
+        {
+            Console.WriteLine("Numbers of " + pair.Key + " : " + pair.Value);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the folder path: ");
+        string path = Console.ReadLine();
+
+        while (!Directory.Exists(path))
+        {
+            Console.WriteLine("There is no such a folder! Try again: ");
+            path = Console.ReadLine();
+        } // check that there is any folder.
+        
+        Console.WriteLine("The folder is found!");
+
+        string[] files = Directory.GetFiles(path, "*.cs"); //get the cs files in the folder.
+        
+        Console.WriteLine("\nFound cs files:");
+        
+        Analyzer analyzer = new Analyzer();
+        analyzer.Analyze(files);
+    }
+}
